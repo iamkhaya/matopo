d9b82bfb0bf91d16306ed827846dca0baa5a34df
Khayelihle Tshuma <khayelihle.tshuma@gmail.com>
2017-08-04 18:30:59 +0200

    Add tests for ability to view offerings

--- spec/features/view_offering_spec.rb
+++ spec/features/view_offering_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 RSpec.feature 'Users can view offerings' do
   before do
-    provider = FactoryGirl.create :provider
-    offering = FactoryGirl.create(:offering, provider: provider)
-
-    visit providers_path(provider)
-    click_link 'View Provider'
+    provider = FactoryGirl.create(:provider)
+    activity = FactoryGirl.create(:activity, id: 1, name: 'Cycle Tours')
+    @offering = FactoryGirl.create(:offering, provider: provider, activity_id:1 )
+    visit offerings_path
+    click_link 'View Offering'
   end
 
   scenario 'for a given project' do
-    click_link 'Cycling'
-    expect(page).to have_content 'This is my cycle activity'
+    expect(page.current_url).to eq offering_url(@offering)
   end
 end
