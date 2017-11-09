018b5b562dbce7e51e91deb938588cd00d8b3fe5
Khayelihle Tshuma <khayelihle.tshuma@gmail.com>
2017-11-09 07:47:28 +0200

    Fixed first test

--- spec/features/admin/edit_category_spec.rb
+++ spec/features/admin/edit_category_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 RSpec.feature 'Users can edit existing categories' do
-  before do
+  before (:each) do
+    puts 'I ran'
     FactoryGirl.create(:category, name: 'Adrenaline')
-    login_as(FactoryGirl.create(:user, :admin))
-    visit '/categories'
-    click_link 'Adrenaline'
-    click_link 'Edit Category'
+    # login_as(FactoryGirl.create(:user, :admin))
+    visit '/admin'
+    fill_in 'Email', with: 'test1@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+
   end
 
   scenario 'with valid attributes' do
+    visit '/categories'
+    click_link 'edit-category'
     fill_in 'Name', with: 'Adrenaline Rush'
     click_button 'Update Category'
     expect(page).to have_content 'Category has been updated.'
@@ -16,6 +22,9 @@ RSpec.feature 'Users can edit existing categories' do
   end
 
   scenario 'with invalid attributes' do
+    visit '/categories'
+    click_link 'edit-category'
+    binding.pry
     fill_in 'Name', with: ''
     click_button 'Update Category'
     expect(page).to have_content 'Category has not been updated.'
